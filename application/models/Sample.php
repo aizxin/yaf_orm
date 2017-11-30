de585717645e2fe04e998a5ab0d50df18c1de158
aizxin <aizxin@gmail.com>
2017-11-30 18:02:31 +0800

    yaf orm

--- application/models/Sample.php
+++ application/models/Sample.php
@@ -4,15 +4,6 @@
  * @desc sample数据获取类, 可以访问数据库，文件，其它系统等
  * @author afoii-12\administrator
  */
-class SampleModel {
-    public function __construct() {
-    }   
-    
-    public function selectSample() {
-        return 'Hello World!';
-    }
-
-    public function insertSample($arrInfo) {
-        return true;
-    }
+class User extends Illuminate\Database\Eloquent\Model {
+	
 }
